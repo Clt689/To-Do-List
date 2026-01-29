e3b8c44dd98697f888b379ad81e8063c02e1350d
Clt689 <kx1302@naver.com>
2026-01-29 12:11:04 +0900

    feat: 삭제 시 alert 추가

--- index.js
+++ index.js
@@ -180,16 +180,20 @@ function editTodo(event) {
 }
 
 function deleteTodo(event) {
-  // todo를 삭제
-  // 1. 선택한 해당 todo의 id 값을 받아와야 함
-  const todoId = event.target.closest("div").dataset.id;
+  const checkDelete = confirm("todo를 삭제하시겠습니까?");
 
-  // 2. todos 배열에서 해당 id 값을 갖고 있는 요소를 splice(인덱스,1)를 통해 삭제
-  const getIndex = todos.findIndex((todo) => todo.id == todoId);
-  todos.splice(getIndex, 1);
+  if (checkDelete) {
+    // 클릭하여 todo를 삭제
+    // 1. 선택한 해당 todo의 id 값을 받아와야 함
+    const todoId = event.target.closest("div").dataset.id;
 
-  // 3. 삭제가 완료되면 renderTodos(todos)를 통해 다시 렌더링
-  renderTodos(todos);
+    // 2. todos 배열에서 해당 id 값을 갖고 있는 요소를 splice(인덱스,1)를 통해 삭제
+    const getIndex = todos.findIndex((todo) => todo.id == todoId);
+    todos.splice(getIndex, 1);
+  
+    // 3. 삭제가 완료되면 renderTodos(todos)를 통해 다시 렌더링
+    renderTodos(todos);
+  }
 }
 
 // 완료 메시지 띄우기
